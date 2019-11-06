9e740f63b0948e182602d74632f0c24b4f2adfa3
Lui Yong Sheng <luiyongsheng@outlook.com>
2019-11-06 14:55:27 +0800

    Environment Variables Setup and Testing

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,20 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <p>{{ token }}</p>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'home',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      token: ''
+    }
+  },
+  mounted() {
+    this.token = process.env.VUE_APP_ACCESS_TOKEN
   }
 }
 </script>
