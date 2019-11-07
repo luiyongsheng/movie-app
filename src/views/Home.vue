8f397ccb73458a4b7959a227f89e26c8498b2c03
Lui Yong Sheng <luiyongsheng@outlook.com>
2019-11-07 16:51:22 +0800

    Video Trailer Implementation (YouTube)

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -13,12 +13,26 @@
                   <i class="icon ion-ios-star leading-none text-yellow-600"></i>
                   <span class="ml-1">{{heroMovie.vote_average}}</span>
                 </p>
-                <h3 class="text-3xl md:text-5xl font-bold text-center leading-none my-3">{{ heroMovie.title }}</h3>
+                <h3
+                  class="text-3xl md:text-5xl font-bold text-center leading-none my-3"
+                >{{ heroMovie.title }}</h3>
                 <p class="text-sm md:text-lg text-center opacity-50">
-                  <span class="px-2" v-for="(g,i) in heroMovie.genre_ids" :key="i">{{getGenresById(g)}} </span>
+                  <span
+                    class="px-2"
+                    v-for="(g,i) in heroMovie.genre_ids"
+                    :key="i"
+                  >{{getGenresById(g)}}</span>
                 </p>
+                <div class="mt-6">
+                  <button class="px-6 py-3 btn-cta rounded" @click="playTrailer">
+                    <i class="icon ion-ios-play mr-3"></i>Watch Trailer
+                  </button>
+                </div>
               </div>
             </div>
+          </div>
+          <div class="tv absolute top-0 left-0 w-full h-full overflow-hidden">
+            <div class="screen mute absolute top-0 right-0 bottom-0 left-0 m-auto" id="tv"></div>
           </div>
         </section>
       </header>
@@ -57,8 +71,20 @@ export default {
   },
   data() {
     return {
-      token: "",
-      apiKey: "",
+      playerSettings: {
+        autoplay: 0,
+        autohide: 1,
+        modestbranding: 0,
+        rel: 0,
+        showinfo: 0,
+        controls: 0,
+        disabledkb: 0,
+        enablejsapi: 0,
+        iv_load_policy: 3
+      },
+      heroVideo: null,
+      tv: null,
+      tvPlaying: false,
       todayPick: [],
       heroMovie: [],
       tvShows: [],
@@ -66,6 +92,34 @@ export default {
     };
   },
   methods: {
+    playerReady() {
+      this.tv.loadVideoById({
+        videoId: this.heroVideo,
+        suggestedQuality: "hd1080"
+      });
+      this.tv.mute();
+    },
+    playTrailer() {
+      document.getElementById("tv").classList.add("active");
+      /*this.tv.loadVideoById({
+        videoId: this.heroVideo,
+        suggestedQuality: "hd1080"
+      });*/
+      this.tv.seekTo(0);
+      this.tv.unMute();
+    },
+    videoRescale() {
+      var w = window.innerWidth,
+        h = window.innerHeight;
+      if (w / h > 16 / 9) {
+        this.tv.setSize(w, (w / 16) * 9);
+        document.getElementById("tv").style.left = "0px";
+      } else {
+        this.tv.setSize((h / 9) * 16, h);
+        var dom = document.getElementById("tv");
+        dom.style.left = `-${dom.outerWidth - w / 2}`;
+      }
+    },
     ...mapActions(["fetchGenres"])
   },
   computed: {
@@ -73,6 +127,8 @@ export default {
   },
   created() {
     this.fetchGenres();
+    window.addEventListener("load", this.videoRescale);
+    window.addEventListener("resize", this.videoRescale);
     axios
       .get(
         `${this.$store.state.api.basePath}/discover/movie?page=1&include_video=true&include_adult=false&sort_by=popularity.desc&language=en-US&api_key=${this.$store.state.api.key}`
@@ -80,6 +136,23 @@ export default {
       .then(async resp => {
         this.todayPick = resp.data.results.slice(0, 20);
         this.heroMovie = this.todayPick[Math.floor(Math.random() * 20) - 1];
+        axios
+          .get(
+            `${this.$store.state.api.basePath}/movie/${this.heroMovie.id}/videos?language=en-US&api_key=${this.$store.state.api.key}`
+          )
+          .then(resp => {
+            var v =
+              resp.data.results[
+                Math.floor(Math.random()) * resp.data.results.length
+              ];
+            this.heroVideo = v.key;
+            this.tv = new window.YT.Player("tv", {
+              events: {
+                onReady: this.playerReady
+              },
+              playerVars: this.playerSettings
+            });
+          });
       });
     axios
       .get(
@@ -114,5 +187,29 @@ export default {
   padding: 4px 8px;
   background: rgba(0, 0, 0, 0.333);
   border-radius: 3px;
+}
+.screen {
+  opacity: 0;
+  transition: opacity 369ms;
+  &.active {
+    opacity: 1;
+  }
+}
+.btn-cta {
+  background: rgba(#2d3748, 0.8);
+  backdrop-filter: saturate(180%) blur(20px);
+
+  box-shadow: 0 0 48px rgba(#2d3748, 0.3618);
+  transition: 128ms ease-out;
+  outline: none !important;
+  @apply border border-transparent font-bold;
+
+  &:hover {
+    background: rgba(#2d3748, 0.5);
+    @apply border-gray-700;
+  }
+  &:active {
+    background: rgba(#48556b, 0.8);
+  }
 }
 </style>
